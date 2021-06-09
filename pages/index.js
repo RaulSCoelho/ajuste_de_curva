9a43ce74ea56f7dc967e26dab9c3e4cffc965025
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-06-09 11:31:22 -0300

    'ajuste_de_curva'

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,46 @@
 import Link from 'next/link'
 
 function Home() {
+    var xn = []
+    var yn = []
     return (<div>
         <header>
-            <h1>Contador</h1>
+            <h1>Ajuste de Curvas</h1>
         </header>
-        <section>
+        <section id="principal">
             <div id="divspace">
-                Início: <input type="number" name="num1" id="num1"></input>(Da contagem)
-            </div>
-            <div id="divspace">
-                Fim: <input type="number" name="num2" id="num2"></input>(Da contagem)
-            </div>
-            <div id="divspace">
-                Contagem: <input type="number" name="cont" id="cont"></input>(De quanto em quanto)
+                Xi: <input type="number" name="xi" id="xi"></input> Yi: <input type="number" name="yi" id="yi"></input>
             </div>
             <div id='btn'>
-                <input id="button" type="button" value="Contar" onClick={() => {
-                    var inicio = Number(document.getElementById('num1').value)
-                    var fim = Number(document.getElementById('num2').value)
-                    var cont = Number(document.getElementById('cont').value)
+            <input id="reset" type="button" value="Resetar" onClick={() => {
                     var res = document.getElementById('res')
-                    if(cont == 0){
-                        res.innerHTML = `[ERRO] COLOQUE O VALOR DO CONTADOR`
-                    }else{
-                        res.innerHTML = ``
-                        if(inicio<fim){
-                            for(inicio; inicio<=fim; inicio+=cont){
-                                if(inicio != fim){
-                                    res.innerHTML += String(inicio) + `\u{1F449}`
-                                }else{
-                                    res.innerHTML += String(inicio) + `\u{270B}`
-                                }
-                            }
-                        }else{
-                            for(inicio; inicio>=fim; inicio-=cont){
-                                if(inicio != fim){
-                                    res.innerHTML += String(inicio) + `\u{1F449}`
-                                }else{
-                                    res.innerHTML += String(inicio) + `\u{270B}`
-                                }
-                            }
+                    res.innerHTML = ``
+                    xn = []
+                    yn = []
+                }}></input><input id="adic" type="button" value="Adicionar" onClick={() => {
+                    var res = document.getElementById('res')
+                    var xitext = document.getElementById('xi').value
+                    var yitext = document.getElementById('yi').value
+                    var xi = Number(xitext)
+                    var yi = Number(yitext)
+                    if (xitext.length == 0 || yitext.length == 0) {
+                        res.innerHTML = `[ERRO] Digite os valores de xi e yi`
+                    } else {
+                        if(res.innerHTML == `[ERRO] Digite os valores de xi e yi`){
+                            res.innerHTML = ``
                         }
+                        res.innerHTML += `${xi} | ${yi}<br>`
+                        xn.push(xi)
+                        yn.push(yi)
                     }
                 }}></input>
             </div>
-            <div id="res">
+            <div id='res'>
 
             </div>
         </section>
         <footer>
-            <p>&copy; RaulSCoelho</p>
+            <p>&copy; Raul Semicek Coelho</p>
         </footer>
     </div>)
 }
