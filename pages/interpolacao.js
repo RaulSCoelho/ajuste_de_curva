e9094c3232499211d4b55a25df9db7ffaec7ea9b
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-06-21 19:19:05 -0300

    'ajuste-de-curva'

--- pages/interpolacao.js
+++ pages/interpolacao.js
@@ -147,10 +147,10 @@ const Interpol = () => {
                         }
                         if (xptext.length != 0 && yptext.length != 0) {
                             res2.innerHTML += `\u{2757} Deixe vazio o campo do valor a ser encontrado \u{2757} <br>`
-                        } else if (xp > maiorx) {
-                            res2.innerHTML += `\u{2757} O valor a ser encontrado deve ser entre ${xn[0]} e ${xn[xn.length - 1]} \u{2757} <br>`
-                        } else if (yp > maiory) {
-                            res2.innerHTML += `\u{2757} O valor a ser encontrado deve ser entre ${yn[0]} e ${yn[yn.length - 1]} \u{2757} <br>`
+                        } else if (xp > maiorx || xp < menorx) {
+                            res2.innerHTML += `\u{2757} O valor a ser encontrado deve ser entre ${menorx} e ${maiorx} \u{2757} <br>`
+                        } else if (yp > maiory || yp < menory) {
+                            res2.innerHTML += `\u{2757} O valor a ser encontrado deve ser entre ${menory} e ${maiory} \u{2757} <br>`
                         } else {
                             res2.innerHTML += `\u{2705} Este valor pode ser usado \u{2705} <br>`
                         }
